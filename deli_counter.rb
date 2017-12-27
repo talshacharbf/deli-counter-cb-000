f445b43788ab422eb486940b0a54296edb0ba04b
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 19:38:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
   else
     katz_deli.each_with_index do |name, index|
       puts "#{index}. #{index}"
-end
+    end
   end
 end
