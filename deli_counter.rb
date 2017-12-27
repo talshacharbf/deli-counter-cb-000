7b1f254a23a7015881db7b25c0b76b550cbfaa76
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 20:31:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,5 +13,5 @@ end
 
 def take_a_number(katz_deli,name)
   line << (name)
-  puts "Welcome, #{name}. You are number #{katz_deli.index(name)} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
