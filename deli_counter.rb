a10ac14c46d9de75b8cc2eb2cc0e6855de659347
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 20:35:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,12 @@ def take_a_number(katz_deli,name)
   katz_deli << (name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name)+1} in line."
 end
+
+def now_serving(katz_deli) #=> "Currently serving Grace."
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Current serving #{katz_deli[0]}"
+  end
+end
+  
