92d51dd0ad963160893feb85f58754b57baaa02a
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 19:29:16 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,5 +5,7 @@ katz_deli = []
 def line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
   if katz_deli = []
     puts "The line is currently empty."
+  else
     puts "The line is currently: #{katz_deli}"
+  end
 end
