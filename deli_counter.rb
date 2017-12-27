d32cfc0785b642be5281e4e40775ce9b3400162d
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 19:27:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,10 @@
 
 katz_deli = []
  
-  take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
+def take_a_number(katz_deli, "Ada") #=> Welcome, Ada. You are number 1 in line.
   take_a_number(katz_deli, "Grace") #=> Welcome, Grace. You are number 2 in line.
   take_a_number(katz_deli, "Kent") #=> Welcome, Kent. You are number 3 in line.
- 
-  line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+
+def line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
+  puts "The line is currently: #{katz_deli}"
  
