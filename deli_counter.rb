933fe19736cd33441e71e287434054b8e3147e02
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 19:52:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,21 +5,8 @@ def line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
   else
     line = "The line is currently:"
     katz_deli.each_with_index do |name, index|
-      line.push(1+index)
-
+      line.push("#{index} #{name}")
     end
     puts line
-  end
-end
-
-def line(deli)
-  if deli.empty?
-    puts "The line is currently empty."
-  else
-    current_line = "The line is currently:"
-    deli.each.with_index(1) do |person, i|
-      current_line << " #{i}. #{person}"
-    end
-    puts current_line
   end
 end
