d4d9ba253fcf42180dd62e5bfcf128d74f87d166
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 20:29:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,8 @@ def line(katz_deli) #=> "The line is currently: 1. Ada 2. Grace 3. Kent"
     puts line
   end
 end
+
+def take_a_number(katz_deli, name)
+  line << (name)
+  puts "Welcome, #{name}. You are number katz_deli.index(name) in line."
+end
